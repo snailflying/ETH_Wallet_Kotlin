3083f4741a6638faf0308da7b476a83023fe0201
Zhiqiang Liu <aaron@magicwindow.cn>
2018-03-09 00:57:31 +0800

    first commit

--- app/src/obj/local/arm64-v8a/objs/scrypt/Users/aaron/iWork/ETHWallet/app/src/c/scrypt_jni.o.d
+++
@@ -1,5 +0,0 @@
-/Users/aaron/iWork/ETHWallet/app/src/obj/local/arm64-v8a/objs/scrypt//Users/aaron/iWork/ETHWallet/app/src/c/scrypt_jni.o: \
-  /Users/aaron/iWork/ETHWallet/app/src/c/scrypt_jni.c \
-  /Users/aaron/iWork/ETHWallet/app/src/include/crypto_scrypt.h
-
-/Users/aaron/iWork/ETHWallet/app/src/include/crypto_scrypt.h:
